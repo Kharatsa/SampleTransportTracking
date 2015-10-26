3e1b1d21c683e84c9e94ff292b4ff058e05e6ef7
Sean Herman <seanherman@gmail.com>
2015-10-26 11:11:50 -0400

    replace pm2 with supervisor

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# sample-tracking
+# Sample Transport Tracking
+
+## Server Installation
+### Credentials
+The app/ directory should contain a `secrets.js` file with the following contents:
+
+    'use strict';
+
+    const ODK_PASSWORD = 'my_password';  // ODK Aggregate password
+    exports.ODK_PASSWORD = ODK_PASSWORD;
+
+This ODK_PASSWORD should provide the user detailed in `config.js` with full access to the ODK Aggregate server.
+
+### Instructions
+First, use rsync (or some other method) to move the `secrets.js` file to the home directory of the server's administrator account. This account will execute the Sample Tracking app, and must have sudo access.
+
+    rsync -avz -e "ssh -i PRIVATE_KEY_PATH" --progress app/secrets.js \
+    USERNAME@HOSTNAME:~;
+
+Execute `bootstrap.sh` on the server over SSH to complete installation.
+
+    ssh -i PRIVATE_KEY_PATH USERNAME@HOSTNAME 'bash -s' < bootstrap.sh
+
+This setup script should launch the Sample Tracking app.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,115 +1,117 @@
 #!/bin/bash
-
 set -x
 
-REMOTE_USER=$1
-REMOTE_HOST=$2
-PRIVATE_KEY_PATH=$3
-
-# Copy the secrets.js file to the server
-rsync -avz -e "ssh -i $PRIVATE_KEY_PATH" --progress app/secrets.js \
-$REMOTE_USER@$REMOTE_HOST:~;
-
-# Bootstrap logic
-ssh -i $PRIVATE_KEY_PATH $REMOTE_USER@$REMOTE_HOST \
-'bash -s' <<'ENDSSH'
-  set -x
-
-  TRACKER_REPO="https://github.com/Kharatsa/sample-tracking.git"
-  SAMPLE_TRACKING_BASEDIR="/srv/www"
-  SAMPLE_TRACKING_DIR="sample-tracking"
-  SAMPLE_TRACKING_PATH="$SAMPLE_TRACKING_BASEDIR/$SAMPLE_TRACKING_DIR"
-  CURRENT_USER_GROUP="$(whoami):$(whoami)"
-  SECRET_DEST="/srv/www/$SAMPLE_TRACKING_DIR/app"
-  NVM_DIR="/opt/nvm"
-  NODE_VERSION="4.2"
-
-  echo "----------------------------------------------"
-  echo "----------------------------------------------"
-  echo "---- BOOTSTRAP SAMPLE TRACKING WEB SERVER ----"
-  echo "----------------------------------------------"
-  echo "----------------------------------------------"
+TRACKER_REPO="https://github.com/Kharatsa/sample-tracking.git"
+SAMPLE_TRACK_BASEDIR="/srv/www"
+SAMPLE_TRACK_DIR="sample-tracking"
+SAMPLE_TRACK_PATH="$SAMPLE_TRACK_BASEDIR/$SAMPLE_TRACK_DIR"
+SAMPLE_TRACK_LOG_PATH="/var/log/$SAMPLE_TRACK_DIR"
+SAMPLE_TRACK_USER="strack"
+SAMPLE_TRACK_USER_GROUP="$SAMPLE_TRACK_USER:$SAMPLE_TRACK_USER"
+CURRENT_USER_GROUP="$(whoami):$(whoami)"
+SECRET_DEST="/srv/www/$SAMPLE_TRACK_DIR/app"
+NVM_DIR="/opt/nvm"
+NODE_VERSION="4.2"
 
 
-  echo "----------------------------------------------"
-  echo "--------- Update system software -------------"
-  echo "----------------------------------------------"
-  sudo apt-get update && sudo apt-get upgrade --yes
+echo "----------------------------------------------"
+echo "----------------------------------------------"
+echo "---- BOOTSTRAP SAMPLE TRACK WEB SERVER -------"
+echo "----------------------------------------------"
+echo "----------------------------------------------"
 
 
-  echo "----------------------------------------------"
-  echo "---- Install build tools, curl, git, vim -----"
-  echo "----------------------------------------------"
-  sudo apt-get install --yes build-essential curl git vim
-
-  echo "----------------------------------------------"
-  echo "------------- Install NVM --------------------"
-  echo "----------------------------------------------"
-  sudo mkdir -p $NVM_DIR
-  sudo chown -R $(whoami):$(whoami) $NVM_DIR
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh \
-  | NVM_DIR=$NVM_DIR bash
-  # Execute nvm.sh to get NVM working in this shell session
-  . $NVM_DIR/nvm.sh
-
-  echo "----------------------------------------------"
-  echo "------------- Install Node.js  ---------------"
-  echo "----------------------------------------------"
-  echo "Using Node.js version $NODE_VERSION"
-  nvm install $NODE_VERSION
-  nvm use $NODE_VERSION
-  # Make NVM's Node.js accessible to all users as just "node"
-  sudo ln -s $(which node) /usr/local/bin/node
-
-  echo "----------------------------------------------"
-  echo "-------- Install gulp, pm2, node-gyp ---------"
-  echo "----------------------------------------------"
-  npm install -g pm2 gulp
+echo "----------------------------------------------"
+echo "--------- Update system software -------------"
+echo "----------------------------------------------"
+sudo apt-get update && sudo apt-get upgrade --yes
 
 
-  # Update .profile to load NVM on boot and use the specified version of Node.js
-  echo ". $NVM_DIR/nvm.sh >/dev/null 2>&1" >> ~/.profile
-  echo "nvm use $NODE_VERSION >/dev/null 2>&1" >> ~/.profile
+echo "----------------------------------------------"
+echo "-------- Install system utilities ------------"
+echo "----------------------------------------------"
+sudo apt-get install --yes build-essential curl git vim supervisor
 
 
-  echo "----------------------------------------------"
-  echo "--------- Configure pm2 for startup ----------"
-  echo "----------------------------------------------"
-  NVM_PM2=$(which pm2)
-  PM2_HOME=/home/$(whoami)/.pm2
-  # pm2 startup requires sudo to successfully update startup scripts
-  sudo PM2_HOME=$PM2_HOME $NVM_PM2 startup -u $(whoami)
-  sudo chown -R $(whoami) $PM2_HOME
+echo "----------------------------------------------"
+echo "------------- Install NVM --------------------"
+echo "----------------------------------------------"
+sudo mkdir -p $NVM_DIR
+sudo chown -R $CURRENT_USER_GROUP $NVM_DIR
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh \
+| NVM_DIR=$NVM_DIR bash
+
+# Run nvm.sh to get NVM working in this shell session
+. $NVM_DIR/nvm.sh
 
 
-  echo "----------------------------------------------"
-  echo "-------- Scaffold Sample Tracker app ---------"
-  echo "----------------------------------------------"
+echo "----------------------------------------------"
+echo "--------- Install Node.js $NODE_VERSION -------------"
+echo "----------------------------------------------"
+nvm install $NODE_VERSION
+nvm use $NODE_VERSION
+NVM_NODE=$(which node)
+NVM_NPM=$(which npm)
 
-  # Establish scaffold for the Sample Tracking app
-  # Create app directories
-  sudo mkdir -p $SAMPLE_TRACKING_PATH
-  sudo chown -R $CURRENT_USER_GROUP $SAMPLE_TRACKING_BASEDIR
+# Make this node and npm version accessible to all users
+sudo ln -s $NVM_NODE /usr/local/bin/node
+sudo ln -s $NVM_NPM /usr/local/bin/npm
 
-  # Drop the Sample Tracking application in place
-  git clone $TRACKER_REPO $SAMPLE_TRACKING_PATH
-  cd $SAMPLE_TRACKING_PATH
-  # Place the secret file
-  mv ~/secrets.js $SAMPLE_TRACKING_PATH/app/
-  npm install
-  cd node_modules
-  # Add a symbolic link to the app so require statements resolve properly
-  sudo ln -s $SAMPLE_TRACKING_PATH/app $SAMPLE_TRACKING_PATH/node_modules/app
+# Update .profile to load NVM on boot and use the specified version of Node.js
+echo ". $NVM_DIR/nvm.sh >/dev/null 2>&1" >> ~/.profile
+echo "nvm use $NODE_VERSION >/dev/null 2>&1" >> ~/.profile
 
-  echo "----------------------------------------------"
-  echo "-------- Launch Sample Tracking app ----------"
-  echo "----------------------------------------------"
-  PM2_HOME=$PM2_HOME pm2 start -n "stracker" /srv/www/$SAMPLE_TRACKING_DIR/app/server/server.js
 
-  echo "----------------------------------------------"
-  echo "-------- Configure app for startup -----------"
-  echo "----------------------------------------------"
-  pm2 dump
+echo "----------------------------------------------"
+echo "----------- Configure Supervisor -------------"
+echo "----------------------------------------------"
+sudo adduser --system --no-create-home --group $SAMPLE_TRACK_USER
+sudo mkdir -p $SAMPLE_TRACK_LOG_PATH
+sudo chown -R $SAMPLE_TRACK_USER_GROUP $SAMPLE_TRACK_LOG_PATH
+sudo chmod -R 770 $SAMPLE_TRACK_LOG_PATH
 
-  sudo reboot
-ENDSSH
+# Create the supervisor configuration file
+sudo sh -c "echo '[program:strack]
+command=$NVM_NODE $SAMPLE_TRACK_PATH/app/server/server.js
+autostart=true
+autorestart=true
+environment=NODE_ENV=production
+stderr_logfile=$SAMPLE_TRACK_LOG_PATH/stracker.err.log
+stdout_logfile=$SAMPLE_TRACK_LOG_PATH/stracker.out.log
+user=$SAMPLE_TRACK_USER' >> /etc/supervisor/conf.d/strack.conf"
+
+
+echo "----------------------------------------------"
+echo "------------- Install gulp -------------------"
+echo "----------------------------------------------"
+npm install -g gulp
+
+
+echo "----------------------------------------------"
+echo "-------- Scaffold Sample Tracker app ---------"
+echo "----------------------------------------------"
+# Create app directories
+sudo mkdir -p $SAMPLE_TRACK_PATH
+sudo chown -R $CURRENT_USER_GROUP $SAMPLE_TRACK_BASEDIR
+
+# Drop the Sample Tracking application in place
+git clone $TRACKER_REPO $SAMPLE_TRACK_PATH
+cd $SAMPLE_TRACK_PATH
+
+# Place the secret file
+mv ~/secrets.js $SAMPLE_TRACK_PATH/app/
+npm install
+cd node_modules
+
+# Add a symbolic link to the app so require statements resolve properly
+sudo ln -s $SAMPLE_TRACK_PATH/app $SAMPLE_TRACK_PATH/node_modules/app
+
+
+echo "----------------------------------------------"
+echo "-------- Launch Sample Tracking app ----------"
+echo "----------------------------------------------"
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start strack
+
+sudo reboot
