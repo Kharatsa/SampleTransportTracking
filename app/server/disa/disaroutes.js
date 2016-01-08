6eecaba48fa87e21932eebd73e964a9334ad2a41
Sean Herman <seanherman@gmail.com>
2016-01-08 14:44:08 -0500

    Small updates to Disa Labs routes to validate MIME type and presence of POST body

--- app/server/disa/disaroutes.js
+++ app/server/disa/disaroutes.js
@@ -10,20 +10,42 @@ const transform = require('app/server/disa/disatransform.js');
 
 // parse application/json
 const textParser = bodyParser.text({
-  type: ['text/xml', 'application/xml']
+  type: '*/xml'
 });
 
-router.put('/metadata',
+function validateMIMEType(req, res, next) {
+  if (req.is('*/xml')) {
+    return next();
+  }
+  var err = new Error('Unsupported MIME type "' + req.get('Content-Type') + '"');
+  err.status = 415;
+  next(err);
+}
+
+function validateBody(req, res, next) {
+  if (req.body && req.body.length) {
+    return next();
+  }
+  var err = new Error('POST body cannot be empty');
+  err.status = 422;
+  next(err);
+}
+
+router.post('/metadata',
   passport.authenticate('basic', {session: false}),
+  validateMIMEType,
   textParser,
+  validateBody,
   function(req, res) {
     // TODO: Update metadata
     res.status(201).send('YOLO');
   });
 
-router.put('/status',
+router.post('/status',
   passport.authenticate('basic', {session: false}),
+  validateMIMEType,
   textParser,
+  validateBody,
   function(req, res, next) {
     log.debug('Received Disa Labs status\n\t', req.body);
     return transform.parseLabStatus(req.body)
