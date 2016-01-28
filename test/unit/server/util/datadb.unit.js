4fd682ed3587aec0e726aa66ad0ea3dc0eaf55c5
Sean Herman <seanherman@gmail.com>
2016-01-28 17:21:58 -0500

    tests for db utilities

--- test/unit/server/util/datadb.unit.js
+++ test/unit/server/util/datadb.unit.js
@@ -3,12 +3,55 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
-// const expect = chai.expect;
-// const dbresult = require('app/server/storage/dbresult.js');
+const expect = chai.expect;
+const dbresult = require('app/server/storage/dbresult.js');
 
-describe('General Purpose Data DB Methods', () => {
-  it('should identify objects with matching props as equal');
-  it('should identify objects with mismatched props as not equal');
-  it('should exclude database date columns from objects');
-  it('should exclude database columns from objects');
+describe('Database Results Helper Functions', () => {
+  const source = {
+    a: 1, b: 2, c: 3
+  };
+  const match = {
+    a: 1, b: 2, c: 3, d: 4, e: 5
+  };
+  it('should identify objects with matching props as equal', () =>
+    expect(dbresult.commonPropsEqual(source, match))
+    .to.equal(true)
+  );
+
+  const missingProps = {
+    a: 1, c: 3, d: 4, e: 5
+  };
+
+  it('should identify objects with missing props as not equal', () =>
+    expect(dbresult.commonPropsEqual(source, missingProps))
+    .to.equal(false)
+  );
+
+  const mismatched = Object.assign({}, match, {b: 99});
+
+  it('should identify objects with mismatched prop values as not equal', () =>
+    expect(dbresult.commonPropsEqual(source, mismatched))
+    .to.equal(false)
+  );
+
+  const raw = {
+    id: 1,
+    uuid: 'xyz',
+    a: 2,
+    createdAt: 'YOLO',
+    updatedAt: 'YOYO'
+  };
+  const dateFiltered = {id: 1, uuid: 'xyz', a: 2};
+
+  it('should exclude database date columns from objects', () =>
+    expect(dbresult.omitDateDBCols(raw))
+    .to.deep.equal(dateFiltered)
+  );
+
+  const allFiltered = {a: 2};
+
+  it('should exclude database columns from objects', () =>
+    expect(dbresult.omitDBCols(raw))
+    .to.deep.equal(allFiltered)
+  );
 });
