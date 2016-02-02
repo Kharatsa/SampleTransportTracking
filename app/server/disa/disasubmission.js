e712fecd58ff930f728cd32141808d17d291cf5a
Sean Herman <seanherman@gmail.com>
2016-02-02 12:15:06 -0500

    removed disa labs * test code expansion support

--- app/server/disa/disasubmission.js
+++ app/server/disa/disasubmission.js
@@ -1,12 +1,8 @@
 'use strict';
 
 const BPromise = require('bluebird');
-const storage = require('app/server/storage');
 const sttsubmission = require('app/server/stt/sttsubmission.js');
 const disatransform = require('app/server/disa/disatransform.js');
-const sttclient = require('app/server/stt/sttclient.js');
-
-const client = sttclient({db: storage.db, models: storage.models});
 
 // TODO: remove
 // const DEBUG = (message, value) => {
@@ -26,9 +22,6 @@ const handleSubmission = incoming => {
   .map(sampleIds => sampleIds.uuid)
   .then(results => results && results.length ? results[0] : {});
 
-  const localTests = sampleIdsRef.then(ref => ({sampleId: ref}))
-  .then(ref => client.labTests.bySampleIds({data: [ref]}));
-
   const testsWithRefs = sampleIdsRef.then(idsRef =>
     disatransform.fillSampleIdRefs(incoming.labTests, idsRef)
   );
@@ -36,14 +29,7 @@ const handleSubmission = incoming => {
   const labTests = testsWithRefs.then(sttsubmission.labTests);
   const allTests = labTests.then(sttsubmission.syncedCombine);
 
-  // The lab tests results used to fill the collection of change records must be
-  // extended to include any existing tests prior to this submission (for the
-  // given sampleId). This accomodates cases where an "*", representing an "all
-  // tests" signifier, is included with a new submission.
-  const extendedTests = BPromise.join(localTests, allTests)
-  .spread((prior, post) => [].concat(prior, post));
-
-  const changes = extendedTests
+  const changes = allTests
   .then(tests => disatransform.fillTestRefs(incoming.changes, tests))
   .then(sttsubmission.labChanges);
 
