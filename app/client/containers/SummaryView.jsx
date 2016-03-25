d1898517c346864a8b89adf5be61cf8ca1438fb3
James <james.kizer@gmail.com>
2016-03-25 19:38:13 -0400

    Added ViewChanges button to navigate to the Changes view

--- app/client/containers/SummaryView.jsx
+++ app/client/containers/SummaryView.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import SummaryFilterContainer from './SummaryFilterContainer';
+import ViewChangesButton from '../components/Summary/ViewChangesButton';
 
 class SummaryView extends React.Component {
   render() {
 
-    var containerStyle = {
-      height: '100%',
-      width: '100%',
-      fontSize: 0
-    }
-    var bothStyle = {
-      display: 'inline-block',
-      zoom: 1,
-      verticalaAlign: 'top',
-      fontSize: 16
-    }
-    var leftStyle = Object.assign({}, bothStyle, {
-      width: '50%'
-    })
-    var rightStyle = Object.assign({}, bothStyle, {
-      width: '50%'
-    })
+    const buttonsStyle = {
+      textAlign: 'right'
+    };
 
     return (
       <div>
@@ -35,11 +22,9 @@ class SummaryView extends React.Component {
             <span>Turn Around Time</span>
           </div>
         </div>
-        <div>
-          View Changes Container
+        <div style={buttonsStyle}>
+          <ViewChangesButton />
         </div>
-
-
       </div>
     );
  }
