c33bd4920397b3ee522e29a82d54a68071dbf823
Sean Herman <seanherman@gmail.com>
2016-08-28 22:13:48 -0400

    completed upgrade to React 15

--- app/client/index.js
+++ app/client/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import Root from './containers/Root';
 import store from './store';
 import './util/memorystats';
-
-// https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin();
 
 ReactDOM.render(
   <Root store={store} />,
