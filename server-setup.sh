ee674accc576915218d740eadd6ae1fa50cebcee
Scot Spinner <sts59@cornell.edu>
2016-11-03 15:01:16 -0400

    updates

--- server-setup.sh
+++ server-setup.sh
@@ -2,10 +2,11 @@
 
 # this script should be run as root
 
+# install docker
 # these instructions come from this document: https://docs.docker.com/engine/installation/linux/ubuntulinux/
 
 apt-get update
-apt-get install apt-transport-https ca-certificates
+apt-get install -y apt-transport-https ca-certificates
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
@@ -13,9 +14,14 @@ apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E8
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update
-apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 
-apt-get install docker-engine
+apt-get install -y docker-engine
 service docker start
 
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.9.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
+# copy db_creds
+cp /etc/db_creds ./docker/env-prod.env
