c3197540e1b9847cd8ba466f850942158f389a75
Sean Herman <seanherman@gmail.com>
2016-11-16 00:38:01 -0500

    more tweaks

--- setup-db.sh
+++ setup-db.sh
@@ -8,6 +8,8 @@ ln -s deploy/docker-compose-prod.yml docker-compose.yml
 # export our db credentials
 export $(cat /etc/stt_creds | xargs)
 
+docker-compose up -d
+
 # create the sst db
 mysql -h $STT_DB_HOST -u $STT_DB_USER --password=$MYSQL_PASSWORD < ./deploy/create_db.sql
 
