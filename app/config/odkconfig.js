a9cfbcfc94162c91a8d86e23e0ef75ea7efb6c56
Sean Herman <seanherman@gmail.com>
2016-11-21 22:51:21 -0500

    bugfix: bad production ODK hostname config

--- app/config/odkconfig.js
+++ app/config/odkconfig.js
@@ -3,11 +3,13 @@
 // ODK Aggregate settings
 const PROTOCOL = process.env.ODK_PROTOCOL || 'http';
 const HOST = process.env.ODK_HOSTNAME;
-const URL = PROTOCOL + '://' + HOST;
+const PORT = process.env.ODK_PORT || 80;
+const PORT_STR = PORT === 80 ? '' : ':' + PORT;
+const URL = `${PROTOCOL}://${HOST}${PORT_STR}`;
 const USERNAME = process.env.ODK_USERNAME || 'admin';
 const PASSWORD = process.env.ODK_PASSWORD || 'password';
 const SUBMISSION_MAX_LENGTH = process.env.SUBMISSION_MAX_LENGTH || 10485760;
 
 module.exports = {
-  PROTOCOL, HOST, URL, USERNAME, PASSWORD, SUBMISSION_MAX_LENGTH
+  PROTOCOL, HOST, URL, PORT, USERNAME, PASSWORD, SUBMISSION_MAX_LENGTH
 };
