19e6efaa92c947445098af6ba21028a34f50cd79
Sean Herman <seanherman@gmail.com>
2016-11-21 23:23:35 -0500

    bugfix: fixed update docker-compose ordering

--- update_deploy.sh
+++ update_deploy.sh
@@ -6,9 +6,9 @@ docker pull kharatsa/strack
 rm docker-compose.yml
 ln -s deploy/docker-compose-prod.yml docker-compose.yml
 
+docker-compose build
+docker-compose up --no-deps --force-recreate -d
+
 if [ $(docker images -q --filter "dangling=true") ]; then
   docker rmi $(docker images -q --filter "dangling=true")
 fi
-
-docker-compose build
-docker-compose up --no-deps --force-recreate -d
